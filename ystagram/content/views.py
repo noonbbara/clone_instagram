baee742972b11345090c1fb5b7291079da1d6702
noonbbara <149371593+noonbbara@users.noreply.github.com>
2024-02-14 01:06:03 +0900

    [UPDATE] DB에서 글을 불러와서 피드에서 조회하는 기능을 추가하였습니다

--- ystagram/content/views.py
+++ ystagram/content/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from .models import Feed
 # Create your views here.
+class Main(APIView):
+    def get(self, request):
+        feed_list = Feed.objects.all().order_by('-id')
+        # select * from content_feed;
+
+        for feed in feed_list:
+            print(feed.content)
+        return render(request, "ystagram/main.html", context=dict(feeds=feed_list))
